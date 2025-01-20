c456092bb77e88fa50914978e87cc88025743270
Azhar_Fikri <azharfikri889@gmail.com>
2025-01-20 10:42:46 +0700

    feat: Layout Home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,12 +8,13 @@ export default function Header() {
         <div className="flex flex-wrap justify-between items-center mx-auto">
           <Link to="/" className="flex items-center">
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
+              // src="https://flowbite.com/docs/images/logo.svg"
+              src="./public/DreamCine.jpeg"
+              className="rounded-full mr-3 h-6 sm:h-9 "
+              alt="Dream Cine Studios"
             />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              BookSales
+              DreamCine - Studios
             </span>
           </Link>
           <div className="flex items-center lg:order-2">
@@ -71,7 +72,7 @@ export default function Header() {
             id="mobile-menu-2"
           >
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>
+              {/* <li>
                 <Link
                   to="/"
                   className="block py-2 pr-4 pl-3 text-white rounded bg-indigo-700 lg:bg-transparent lg:text-indigo-700 lg:p-0 dark:text-white"
@@ -79,31 +80,35 @@ export default function Header() {
                 >
                   Home
                 </Link>
-              </li>
-              <li>
+              </li> */}
+
+              {/* <li>
                 <Link
                   to="/books"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-indigo-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Book
+                  Movies
                 </Link>
-              </li>
-              <li>
+              </li> */}
+              
+              {/* <li>
                 <Link
                   to="/teams"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-indigo-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Team
                 </Link>
-              </li>
-              <li>
+              </li> */}
+
+              {/* <li>
                 <Link
                   to="/contacts"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-indigo-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Contact
                 </Link>
-              </li>
+              </li> */}
+
             </ul>
           </div>
         </div>
