18d3709581cbf46c10ceebde78c7824c9254cd4c
Ar536 <ariqmuhammadfathin2@gmail.com>
2025-01-22 09:19:26 +0700

    feat: Paymnet Method

--- src/pages/admin/payment_methods/edit.jsx
+++ src/pages/admin/payment_methods/edit.jsx
@@ -1,6 +1,65 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getPaymentmethods, updatePaymentmethod } from "../../../services/paymentMethod";
 
 export default function PaymentMethodEdit() {
   
+  const[errors,setErrors] = useState({})
+   
+
+  const [name, setName] = useState(''); //ush
+  const [account_number, setAccountNumber] = useState('');
+
+  //Destruct ID dari URL
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  //fetch datanbuku berdasarkan ID
+  const fetchPmethodDetails = async () => {
+     const data = await getPaymentmethods() // ambil semua data buku
+
+     //cari data buku berdasarkan ID
+     const paymentMethod =data.find(paymentMethod => paymentMethod.id === parseInt(id)) //find itu mencari
+     if (paymentMethod) {
+          //Assign data to state
+          setName(paymentMethod.name)
+          setAccountNumber(paymentMethod.account_number)
+     } 
+  //    console.log(pmethod)
+  }
+
+  useEffect(() => {
+    fetchPmethodDetails()
+}, []);
+
+
+  //upload pmethod data
+  const updatePaymentMethodDetails = async(e) => { //utk form submit
+      e.preventDefault()
+
+      //buat FormData
+      const PaymentMethodData = new FormData()
+
+      PaymentMethodData.append('name', name)
+      PaymentMethodData.append('account_number', account_number)
+      PaymentMethodData.append('_method', 'PUT')
+      
+
+      await updatePaymentmethod(id, PaymentMethodData)
+      .then(() => {
+          // redirect ke halaman index
+          navigate('/admin/payment_methods')
+          console.log('data', PaymentMethodData)
+      }) 
+      .catch((err) => {
+          console.log(err.response.data.message)
+          setErrors(err.response.data.message)
+      })
+
+      // console.log(PaymentMethodData)
+      
+  }
+    
 
   return (
     <div className="flex flex-col gap-9">
@@ -14,7 +73,7 @@ export default function PaymentMethodEdit() {
             Edit Genres
           </h3>
         </div>
-        <form className="py-5">
+        <form onSubmit={updatePaymentMethodDetails} className="py-5">
           <div className="p-6.5 flex flex-col gap-5">
             
             <div className="mb-4.5">
@@ -23,10 +82,16 @@ export default function PaymentMethodEdit() {
               >
                 Name
               </label>
-              
+              {errors.name &&(
+                <div className="p-2 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
+                    <span className="font-medium">{errors.name[0]}</span>
+                </div>
+              )}
               <input
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 type="text"
-              
                 className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-indigo-600"
               />
             </div>
@@ -37,12 +102,15 @@ export default function PaymentMethodEdit() {
               >
                 Account Number
               </label>
-                <div className="p-2 my-2 text-sm text-red-800 rounded-lg bg-red-50">
-                    <span className="font-medium"></span>
+              {errors.account_number &&(
+                <div className="p-2 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
+                    <span className="font-medium">{errors.account_number[0]}</span>
                 </div>
-              
+              )}
               <textarea
-
+                name="account_number"
+                value={account_number}
+                onChange={(e) => setAccountNumber(e.target.value)}
                 rows="6"
                 className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-indigo-600 active:border-indigo-600 disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-indigo-600"
               ></textarea>
--- src/pages/admin/payment_methods/index.jsx
+++ src/pages/admin/payment_methods/index.jsx
@@ -2,10 +2,33 @@
 // import { useEffect, useState } from "react"
 // import { getPaymentMethods } from "../../../services/paymentMethods";
 import { Link } from "react-router-dom";
-import paymentMethods from "../../../utils/constants/paymentMethods";
+// import paymentMethods from "../../../utils/constants/paymentMethods";
+import { useEffect, useState } from "react";
+import {deletePaymentmethod, getPaymentmethods} from "../../../services/paymentMethod";
 
 export default function PayMethods() {
  
+  const[paymentMethods, setPaymentMethods] = useState([])
+
+    useEffect(() => {
+        const fetchPmethods = async () =>{
+            const data = await getPaymentmethods()
+            setPaymentMethods(data)
+        }
+        fetchPmethods()
+    }, [])
+
+    const handleDelete = async (id) => {
+        const confirmedDelete = window.confirm("Apakah Anda yakin ingin menghapus data ini")
+    
+        if (confirmedDelete){
+            await deletePaymentmethod(id)
+            setPaymentMethods(paymentMethods.filter(paymentmethod => paymentmethod.id !==id))
+            }
+        }
+        
+    
+    // console.log(pmethods)
   
   return (
     <div
@@ -40,15 +63,15 @@ export default function PayMethods() {
           <tbody>
 
         
-          {paymentMethods.map((pm) => ( 
-            <tr key={pm.id} className="hover:bg-gray-50">
+          {paymentMethods.map((payMentmethod) => ( 
+            <tr key={payMentmethod.id} className="hover:bg-gray-50">
             <td
               className="px-4 py-5 pl-9 xl:pl-11"
             >
-              <h5 className="font-medium text-black dark:text-white">{pm.name}</h5>         
+              <h5 className="font-medium text-black dark:text-white">{payMentmethod.name}</h5>         
             </td>
             <td className="px-4 py-5">
-              <p className="text-black dark:text-white">{pm.account_number}</p>
+              <p className="text-black dark:text-white">{payMentmethod.account_number}</p>
               
             </td>
 
@@ -56,8 +79,8 @@ export default function PayMethods() {
             <td className="px-4 py-5">
               <div className="flex items-center space-x-3.5">
                 <Link to="/admin/payment_methods/create"><i className="fa-solid fa-plus"></i></Link>
-                <Link to={`/admin/payment_methods/edit/${pm.name}`}><i className="fa-solid fa-pen-to-square"></i></Link>
-                <button>
+                <Link to={`/admin/payment_methods/edit/${payMentmethod.id}`}><i className="fa-solid fa-pen-to-square"></i></Link>
+                <button onClick={()=> handleDelete(payMentmethod.id)}>
                   <i className="fa-solid fa-trash"></i>
                 </button>
               </div>
