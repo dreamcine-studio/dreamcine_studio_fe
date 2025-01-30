a84c7bb8e0857aed72d8fb45446a697708ac47f9
Azhar_Fikri <azharfikri889@gmail.com>
2025-01-30 11:27:07 +0700

    color

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,7 +2,7 @@
 export default function Contact() {
     return (
       <div className="relative flex items-top justify-center min-h-screen bg-white dark:bg-gray-900 sm:items-center sm:pt-0">
-          <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-gray-300 max-w-6xl mx-auto sm:px-6 lg:px-8">
               <div className="mt-8 overflow-hidden">
                   <div className="grid grid-cols-1 md:grid-cols-2">
                       <div className="p-6 mr-2 bg-gray-100 dark:bg-gray-800 sm:rounded-lg">
