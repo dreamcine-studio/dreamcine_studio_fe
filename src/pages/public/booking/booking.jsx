6712e648d1461e45e7cbf0fe4bd21e7f25a7010b
hiphipdora <faaay.adistiii@gmail.com>
2025-02-03 10:56:51 +0700

    fix: booking seat token

--- src/pages/public/booking/booking.jsx
+++ src/pages/public/booking/booking.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import { getMovies } from "../../../services/movies";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getStudios } from "../../../services/studios";
+import { updateBooking } from "../../../services/booking";
 
 export default function BookingPublic() {
   const [selectedSeats, setSelectedSeats] = useState(0);
   const [movie, setMovie] = useState([]);
+  const [studio, setStudio] = useState([]);
   const location = useLocation();
+  const navigate = useNavigate();
 
   const query = new URLSearchParams(location.search);
   const movieId = query.get("movie_id");
@@ -25,8 +29,21 @@ export default function BookingPublic() {
         // Handle error, e.g., display a message to the user
       }
     };
+
+    const fetchStudio = async () => {
+      try {
+        const data = await getStudios();
+        const studio = data.find((studio) => studio.id === parseInt(studioId));
+        setStudio(studio);
+      } catch (error) {
+        console.error("Error fetching studio:", error);
+        // Handle error, e.g., display a message to the user
+      }
+    }
+
     fetchMovie();
-  }, [movieId]);
+    fetchStudio();
+  }, [movieId, studioId]);
 
   const handleSeatClick = (e) => {
     if (!e.target.classList.contains("sold")) {
@@ -49,6 +66,36 @@ export default function BookingPublic() {
 
   const totalPrice = movie.price * selectedSeats;
 
+  const updateBookingDetails = async (e) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem("token");
+    if (!token) {
+      alert("You must log in to place an order.");
+      const currentURL = `/moviebooking?movie_id=${movieId}&studio_id=${studioId}&showtime=${showtime}&showdate_start=${showdate_start}`;
+      return navigate("/login", { state: { from: currentURL } });
+    }
+
+    const bookingData = new FormData();
+    bookingData.append("movie_id", movieId);
+    bookingData.append("studio_id", studioId);
+    bookingData.append("showtime", showtime);
+    bookingData.append("showdate_start", showdate_start);
+    bookingData.append("quantity", selectedSeats);
+    bookingData.append("total_price", totalPrice);
+    bookingData.append('_method', 'PUT')
+    
+    try {
+      await updateBooking(bookingData);
+      return navigate("#");
+    } catch (err) {
+      // console.log(err.response.data.message);
+      setErrors(err.response.data.message);
+    }
+
+  }
+  
+
   return (
     <div className="flex flex-col items-center justify-center dark:bg-gray-900 text-white w-full p-8">
       <div className="w-full flex items-center justify-center">
@@ -57,13 +104,15 @@ export default function BookingPublic() {
             <thead className="text-sm text-black uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-200 text-left">
               <tr>
                 <th className="px-2 py-4">Movie</th>
+                <th className="px-2 py-4">Studio</th>
                 <th className="px-2 py-4">Time</th>
                 <th className="px-2 py-4">Date</th>
               </tr>
             </thead>
             <tbody className="text-sm text-gray-700 dark:text-gray-400">
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer">
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <td className="px-2 py-2">{movie.title}</td>
+                <td className="px-2 py-2">{studio.name}</td>
                 <td className="px-2 py-2">{showtime}</td>
                 <td className="px-2 py-2">{showdate_start}</td>
               </tr>
@@ -140,7 +189,7 @@ export default function BookingPublic() {
         </div>
       </div>
 
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6">
+      <button onClick={updateBookingDetails} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6">
         Book Your Ticket
       </button>
     </div>
