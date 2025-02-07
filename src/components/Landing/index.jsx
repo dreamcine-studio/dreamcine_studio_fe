4027ae23949b279ad7432c0ef6f494812c9b79fb
hiphipdora <faaay.adistiii@gmail.com>
2025-02-07 21:50:31 +0700

    fix: landing mt-24

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,7 +1,7 @@
 export default function Landing() {
   return (
     <>
-      <section className="dark:bg-gray-900">
+      <section className="dark:bg-gray-900 mt-24">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
           <div className="mr-auto place-self-center lg:col-span-7 sm:p-8 ">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
