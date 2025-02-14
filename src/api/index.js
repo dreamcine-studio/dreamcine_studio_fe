70ac0f4e502230d46ed1b72c02b11cd9af71beff
hiphipdora <faaay.adistiii@gmail.com>
2025-02-14 18:55:36 +0700

    fix: api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const url = 'https://api-studio.karyakreasi.id/'
-const url = 'https://apistudio-fathqulbc.karyakreasi.id/'
+const url = 'https://apistudio-fathqulbc.karyakreasi.id'
 
 export const API = axios.create({
   baseURL: `${url}/api`
